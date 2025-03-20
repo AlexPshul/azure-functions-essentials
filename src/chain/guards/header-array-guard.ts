65babd82673e5f3ee9b43e17911510d0e6b8fb54
Alex Pshul <alex@pshul.com>
2025-03-20 14:02:48 -0400

    Added atLeastOneHeaderGuard

--- src/chain/guards/header-array-guard.ts
+++ src/chain/guards/header-array-guard.ts
@@ -28,3 +28,17 @@ export const exactValuesHeaderGuard = (header: string, values: string[], separat
 
     return headerArray.length === values.length && values.every(value => headerArray.includes(value));
   });
+
+/**
+ * Checks if at least one of the specified values is present in the given header.
+ * @param header - The name of the header to check
+ * @param values - The values to check for in the header
+ * @param separator - The separator used to split the header values
+ * @return A guard function that validates the presence of values in the header
+ * */
+export const atLeastOneHeaderGuard = (header: string, values: string[], separator = ',') =>
+  guard(req => {
+    const headerArray = getHeaderArray(req, header, separator);
+
+    return values.some(value => headerArray.includes(value));
+  });
