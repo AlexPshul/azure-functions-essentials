7db745ff54bc6372c046ac3ad6485b624ae0f5a9
Alex Pshul <alex@pshul.com>
2025-03-20 14:01:37 -0400

    Added exact values header guard

--- src/chain/guards/header-array-guard.ts
+++ src/chain/guards/header-array-guard.ts
@@ -8,9 +8,23 @@ import { guard } from './guard';
  * @param separator - The separator used to split the header values
  * @returns A guard function that validates the presence of values in the header
  */
-export const headerAllValuesGuard = (header: string, values: string[], separator = ',') =>
+export const allValuesHeaderGuard = (header: string, values: string[], separator = ',') =>
   guard(req => {
     const headerArray = getHeaderArray(req, header, separator);
 
     return values.every(value => headerArray.includes(value));
+  });
+
+/**
+ * Checks if the header contains all specified values and no extra values. (Order is ignored)
+ * @param header - The name of the header to check
+ * @param values - The values to check for in the header
+ * @param separator - The separator used to split the header values
+ * @returns A guard function that validates the presence of values in the header
+ */
+export const exactValuesHeaderGuard = (header: string, values: string[], separator = ',') =>
+  guard(req => {
+    const headerArray = getHeaderArray(req, header, separator);
+
+    return headerArray.length === values.length && values.every(value => headerArray.includes(value));
   });
