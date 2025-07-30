7fa22e286c7d59705bff158c5c3f79ea88e5250d
Alex Pshul <alex@pshul.com>
2025-07-30 01:13:14 -0400

    Added code commits for easier understanding of the get vs. getNamed and create vs. createNamed

--- src/chain/input-factory.ts
+++ src/chain/input-factory.ts
@@ -2,11 +2,33 @@ import { InvocationContext } from '@azure/functions';
 import { InputBindingSetter } from './types';
 
 type Getters<TResult> = {
+  /**
+   * Gets the input data from the context.
+   * @param context - The invocation context
+   * @returns The input data from the binding
+   */
   get: (context: InvocationContext) => TResult;
+  /**
+   * Gets the input data from the context using a specific name.
+   * @param context - The invocation context
+   * @param name - The name of the input binding to differentiate it in the context from same typed bindings
+   * @returns The input data from the binding
+   */
   getNamed: (context: InvocationContext, name: string) => TResult;
 };
 type Creators<TArgs> = {
+  /**
+   * Creates a new input binding instance with the provided arguments.
+   * @param args - The arguments to create the input binding
+   * @returns An InputBindingSetter that can be used to set data in the context. Can be passed in the chain inside `useInputBinding`.
+   */
   create: (args: TArgs) => InputBindingSetter;
+  /**
+   * Creates a named input binding instance with the provided arguments.
+   * @param args - The arguments to create the input binding
+   * @param name - The name of the input binding to differentiate it in the context from same typed bindings
+   * @returns An InputBindingSetter that can be used to set data in the context. Can be passed in the chain inside `useInputBinding`.
+   */
   createNamed: (args: TArgs, name: string) => InputBindingSetter;
 };
 
@@ -27,6 +49,18 @@ type InputBinding<TArgs, TResult> = Getters<TResult> & Creators<TArgs>;
  *     .handle((_, context) => funcResult('Ok', upperCaseInput.get(context)));
  * });
  *
+ * // Or a named input binding that allows for same binding with different params
+ * app.get('/to-upper-case-named', {
+ *  handler: startChain()
+ *    .useInputBinding(({ request }) => upperCaseInput.createNamed(getQuery(request, 'firstName'), 'nameA'))
+ *    .useInputBinding(({ request }) => upperCaseInput.createNamed(getQuery(request, 'lastName'), 'nameB'))
+ *    .handle((_, context) => {
+ *      const upperCaseFirstName = upperCaseInput.getNamed(context, 'nameA');
+ *      const upperCaseLastName = upperCaseInput.getNamed(context, 'nameB');
+ *      return funcResult('Ok', { upperCaseFirstName, upperCaseLastName });
+ *    });
+ * });
+ *
  * // GET: http://localhost:7071/api/to-upper-case?text=hello
  * // RESPONSE: { status: 200, body: 'HELLO' }
  */
@@ -41,6 +75,7 @@ export const inputFactory = <TArgs, TResult>(key: string, dataFetch: (args: TArg
   });
 
   return {
+    /// Gets the input data from the context using the default name
     getNamed,
     get: context => getNamed(context, 'default'),
     createNamed,
